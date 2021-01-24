617bff217e579c2d051d32043326c1f4544db0f8
boorooksus <boorooksus@gmail.com>
2021-01-24 16:17:55 +0900

    정적 컨텐츠, mvc

---
+++ Hello_Spring_Boot/src/main/java/Spring_Boot_Study/Hello_Spring_Boot/HelloSpringBootApplication.java
@@ -0,0 +1,13 @@
+package Spring_Boot_Study.Hello_Spring_Boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringBootApplication.class, args);
+	}
+
+}
---
+++ Hello_Spring_Boot/src/test/java/Spring_Boot_Study/Hello_Spring_Boot/HelloSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package Spring_Boot_Study.Hello_Spring_Boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
